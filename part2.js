2b25b6e388dca5e7f6ad51c0532e6f3b7bca15fc
RU-019 <RU-019@Admins-Mac-mini-61.local>
2013-10-11 17:57:01 -0600

    first commit

---
+++ part2.js
@@ -0,0 +1,22 @@
+// Returns a string that concatenates each string from the array separated by 
+// the given delimeter.
+// e.g. join(['one', 'two', 'three'], ' and ') 
+// returns: 'one and two and three'
+// var join = function(arr, delimeter) {
+//  var output = '';
+//  for(var i=0; i<arr.length; i++) {
+//   output += arr[i] + delimeter;
+//  }
+//  return output;
+// };
+
+var join = function(arr, delimeter) {
+ 
+ for(var i=0; i<arr.length; i++) {
+  var  output += arr[i];
+  if (i != arr.length -1) {
+  	output += delimeter;
+  }
+ }
+ return output;
+};
